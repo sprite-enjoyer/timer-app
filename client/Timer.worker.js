ba883dfcaa4560311638966643de5ec14fd97623
sprite-enjoyer <samkharadze.saba01@gmail.com>
2024-01-07 16:54:18 +0400

    discarded timer worker logic

--- client/Timer.worker.js
+++ client/Timer.worker.js
@@ -1,41 +1,20 @@
-// type EventData = { duration: number }; Duration is ms length of timer you want to start
+// type EventData = { duration: number, updateInterval }; Duration is ms length of timer you want to start
 
 self.onmessage = (event) => {
   switch (event.data.type) {
     case "startTimer":
       handleStartTimer(event);
       break;
-    case "stopTimer":
-      handleStopTimer();
-      break;
     case "pauseTimer":
-      handlePauseTimer();
+      handlePauseTimer(event);
       break;
   }
 };
 
-function handlePauseTimer() {}
-
-function handleStopTimer() {
-  clearInterval(interval);
+function handlePauseTimer(event) {
+  //TODO
 }
 
 function handleStartTimer(event) {
-  const duration = event.data.duration;
-  if (!isNaN(duration)) {
-    const durationSeconds = duration / 1000;
-    let elapsedTime = durationSeconds;
-
-    startTimer(() => {
-      elapsedTime--;
-      postMessage({ elapsedTime });
-      if (elapsedTime <= 0) {
-        handleStopTimer();
-      }
-    }, duration);
-  }
-}
-
-async function startTimer(secondCallback) {
-  interval = setInterval(secondCallback, 1000);
+  //TODO
 }
